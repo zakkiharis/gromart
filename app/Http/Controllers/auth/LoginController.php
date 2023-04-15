173341ba2a5a1b5d2d7396d06f3514f5334c99f4
Zakki Haris <92501896+zakkiharis@users.noreply.github.com>
2023-04-15 22:00:28 +0700

    initial commit gromart

---
+++ app/Http/Controllers/auth/LoginController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Inertia\Inertia;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return Inertia::render('admin/auth/login', [
+            "title" => 'Grocery | Login'
+        ]);
+    }
+}
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Grocery</title>  
+        <title inertia>{{ config('app.name', 'Grocery') }}</title>  
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
